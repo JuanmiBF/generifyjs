86e8e2422fe1fcb657d562974006a08982f93a2e
Juan Miguel <juanmiblancof@gmail.com>
2019-06-23 17:44:25 +0200

    Deleted DB settings from model

--- src/models/customer.model.js
+++ src/models/customer.model.js
@@ -1,14 +1,5 @@
 let mongoose = require('mongoose')
 
-//const SERVER = 'elongo-bd-adlff.mongodb.net';
-//const DATABASE = 'test';
-//const USER = 'db_admin';
-//const PASSWORD = '1MUJ9nwbZw5GwnqK';
-//const OPTIONS = 'retryWrites=true&w=majority';
-
-
-//mongoose.connect(`mongodb+srv://${USER}:${PASSWORD}@${SERVER}/${DATABASE}?${OPTIONS}`);
-
 let CustomerSchema = new mongoose.Schema({
     name: String,
     email: {
